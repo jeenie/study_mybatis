ad9cf553b089423dd258e5475c4a26c0bd47f68f
jeenie <jeenie828@gmail.com>
2018-11-01 00:05:08 +0900

    dynamicSQL

---
+++ src/main/java/net/skhu/dto/User.java
@@ -0,0 +1,60 @@
+package net.skhu.dto;
+
+public class User {
+	int id;
+	String userid;
+	String pasword;
+	String name;
+	String email;
+	int departmentId;
+	boolean enabled;
+	String userType;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String uerid) {
+		this.userid = uerid;
+	}
+	public String getPasword() {
+		return pasword;
+	}
+	public void setPasword(String pasword) {
+		this.pasword = pasword;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+}
